fc676fe8611f0dd5dd54331be7d90c613e65a06d
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-06-05 13:40:57 +0530

    Comment line is added

--- indices_of_1_element.py
+++ indices_of_1_element.py
@@ -1,5 +1,8 @@
 #4.Find the index of all 1 elements:-
 
+#Method-1
+
+
 inp_list=[1,2,4,5,1,1,4,1,56]
 indices_list=[]
 for i in range(0,len(inp_list)):
@@ -7,7 +10,7 @@ for i in range(0,len(inp_list)):
         indices_list.append(i)
 print("Indexes of 1 elements: ",indices_list)
 
-#user input
+#Method-2 user input
 
 inp_list=[int(x) for x in input().split(",")]
 inp_list=list(inp_list)
